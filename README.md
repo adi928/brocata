615a8e547d194dbf3eb9e5378a003b5dd24b6b10
Aditya Nath <928.aditya@gmail.com>
2019-05-30 11:00:57 -0400

    1. Modified the ssl-intel--to-dat file converter to generate data files for both: SSL blacklists, JA3 Fingerprints and Ransomware tracker 2. removed unwanted files. 3. updated Readme.md

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 ## Brocata
 Porting Suricata to Bro signatures
 
+Update: The script has been completely automated from end-to-end which means,
+it doesn't need an argument anymore. It downloads the blacklists, rules from the provided 
+urls, giving appropriate error messages if the link is buggy.
+
 In this example it is converting CVE 2019-0708 rule
 
-    $ python brocata.py cve_2019_0708.rules
+    $ python brocata.py
     
     signature cve-2019-0708 {
         src-ip == any
@@ -15,7 +19,6 @@ In this example it is converting CVE 2019-0708 rule
         tcp-state originator,established
         event "NCCGROUP RDP connection setup with MS_T120 channel potential CVE-2019-0708"
     }
-    ...
 
 Included within are the following attributes from Suricata format:
 * Ports
@@ -30,16 +33,14 @@ Included within are the following attributes from Suricata format:
     * http_request_header
     * http_reply_header
     * http_request_body
-    
-`sig_output` Location for all the generated signatures
 
-## Ja3Intel.py
-* Converts the suricata ruleset from abuse.ch to intel format of Bro's Intelligence framework
+Update: The generated signatures are now stored within a single file within the 
+bro site folder (`$Prefix/bro/share/bro/site/`) within the folder name `suricata_rules`.
 
-    ``alert tls any any -> any any (msg:"SSLBL: Malicious JA3 SSL-Client Fingerprint detected (Tofsee)"; ja3_hash; content:"906004246f3ba5e755b043c057254a29"; reference:url, sslbl.abuse.ch/ja3-fingerprints/906004246f3ba5e755b043c057254a29/; sid:906200000; rev:1;)``
-    
-    to
-    
-    ``#fields	indicator	indicator_type	meta.url
-906004246f3ba5e755b043c057254a29	Intel::JA3	sslbl.abuse.ch/ja3-fingerprints/906004246f3ba5e755b043c057254a29/
-`` 
+The script also generates the __load__.bro within the same folder.
+
+The only thing user needs to manually do is add `@load suricata_rules` in local.bro where Bro site is.
+
+## blacklist-intel.py
+* Converts the SSL, JA3 fingerprints and Ransomeware data from CSV, obtained from abuse.ch 
+to intel files which can be used in Intel framework of Bro IDS.
--- blacklist-intel.py
+++ blacklist-intel.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python3
 
 import sys
-
 import requests
 
 sslURL = 'https://sslbl.abuse.ch/blacklist/sslblacklist.csv'
-sslBlocklistCSV = 'sslblicklist.csv'
+sslBlocklistCSV = 'sslBlocklist.csv'
 sslBlocklistDAT = 'sslBlocklist.dat'
 
-ransomwareUrl = 'https://ransomwaretracker.abuse.ch/feeds/csv/'
+ja3URL = 'https://sslbl.abuse.ch/blacklist/ja3_fingerprints.csv'
+ja3CSV = 'ja3Blocklist.csv'
+ja3DAT = 'ja3Blocklist.dat'
+
+ransomwareURL = 'https://ransomwaretracker.abuse.ch/feeds/csv/'
 ransomewareCSV = 'ransomewaretracker.csv'
 ransomewareDAT = 'ransomewaretracker.dat'
 
-def main():
+def sslBlockList():
     # Download the ssl blocklist feeds
     try:
         r = requests.get(sslURL, allow_redirects=True)
@@ -39,7 +42,7 @@ def main():
         writeFile = open(sslBlocklistDAT, 'w+')
         for line in cvsfile:
             if line.startswith('# Listing') or lineCount >= 1:
-                row = line.replace('#','').replace('"', '')
+                row = line.replace('#', '').replace('"', '')
                 sslComp = row.split(',')
                 if sslComp.__len__() <= 1:
                     continue
@@ -47,13 +50,58 @@ def main():
                     writeFile.write('#fields\tindicator\tindicator_type\tmeta.url\tmeta.source\n')
                     lineCount += 1
                 else:
-                    writeFile.write(sslComp[1] + '\tIntel::FILE_HASH' + '\thttps://sslbl.abuse.ch/ssl-certificates/sha1/'\
-                                    + sslComp[1] + '\tabuse.ch\n')
+                    writeFile.write(
+                        sslComp[1] + '\tIntel::FILE_HASH' + '\thttps://sslbl.abuse.ch/ssl-certificates/sha1/' \
+                        + sslComp[1] + '\tabuse.ch\n')
                     lineCount += 1
 
+
+def ja3Fingerprint():
+    # Download the JA3 blocklist feeds
+    try:
+        r = requests.get(ja3URL, allow_redirects=True)
+    except requests.HTTPError as err:
+        print("An HTTP error occured:\n" + err)
+        sys.exit()
+    except requests.Timeout as err:
+        print("The request timed out.\n" + err)
+        sys.exit()
+    except requests.RequestException as err:
+        print("The request had something really bad with it. Bailing now...\n" + err)
+        sys.exit()
+    except requests.ConnectionError as err:
+        print("The connection has not been established and disconnected with an error\n" + err)
+        sys.exit()
+    except requests.SSLError as err:
+        print("The connection has SSL error\n" + err)
+        sys.exit()
+
+    open(ja3CSV, 'wb').write(r.content)
+
+    with open(ja3CSV, newline='', encoding='ISO-8859-1') as cvsfile:
+        lineCount = 0
+        ja3writeFile = open(ja3DAT, 'w+')
+        for line in cvsfile:
+            if line.startswith('# ja3') or lineCount >= 1:
+                row = line.replace('#', '').replace('"', '')
+                ja3Comp = row.split(',')
+                if ja3Comp.__len__() <= 1:
+                    continue
+                if lineCount == 0:
+                    ja3writeFile.write('#fields\tindicator\tindicator_type\tmeta.url\tmeta.source\n')
+                    lineCount += 1
+                else:
+                    ja3writeFile.write(
+                        ja3Comp[0] + '\tIntel::JA3' + '\thttps://sslbl.abuse.ch/ja3-fingerprints/' \
+                        + ja3Comp[0] + '\tabuse.ch\n')
+                    lineCount += 1
+        ja3writeFile.close()
+
+
+def ransomewareBlocklist():
     # Download the ransomware blocklist feeds
     try:
-        r = requests.get(ransomwareUrl, allow_redirects=True)
+        r = requests.get(ransomwareURL, allow_redirects=True)
     except requests.HTTPError as err:
         print("An HTTP error occured:\n" + err)
         sys.exit()
@@ -74,15 +122,15 @@ def main():
 
     with open(ransomewareCSV, newline='', encoding='ISO-8859-1') as csvfile:
         lineCount = 0
-        writeFile = open(ransomewareDAT, 'w+')
+        rwWriteFile = open(ransomewareDAT, 'w+')
         for line in csvfile:
             if line.startswith('# Firstseen') or lineCount >= 1:
-                line = line.replace('#','').replace('"', '')
+                line = line.replace('#', '').replace('"', '')
                 components = line.split(',')
                 if components.__len__() <= 1:
                     continue
                 if lineCount == 0:
-                    writeFile.write('#fields\tindicator\tindicator_type\tmeta.url\tmeta.source\n')
+                    rwWriteFile.write('#fields\tindicator\tindicator_type\tmeta.url\tmeta.source\n')
                     lineCount += 1
                 else:
                     domain = ''
@@ -95,23 +143,33 @@ def main():
 
                     for ip in ips:
                         ipIntelStr = ip + '\tIntel::ADDR' + '\thttps://ransomwaretracker.abuse.ch/feeds/' + '\t' \
-                                     + components[1]+'-'+components[2]+components[6]+'-'+components[8]+'-'+components[9]
-                        writeFile.write(ipIntelStr)
+                                     + components[1] + '-' + components[2] + components[6] + '-' + components[8] + '-' + \
+                                     components[9]
+                        rwWriteFile.write(ipIntelStr)
 
                     if domain != '':
                         domainIntelStr = domain + '\tIntel::DOMAIN' + '\thttps://ransomwaretracker.abuse.ch/feeds/' + '\t' \
-                                     + components[1] + '-' + components[2] + components[6] + '-' + components[8] + '-' + \
-                                     components[9]
-                        writeFile.write(domainIntelStr)
+                                         + components[1] + '-' + components[2] + components[6] + '-' + components[
+                                             8] + '-' + \
+                                         components[9]
+                        rwWriteFile.write(domainIntelStr)
 
                     if url != '':
                         urlIntelStr = url + '\tIntel::URL' + '\thttps://ransomwaretracker.abuse.ch/feeds/' + '\t' \
-                                     + components[1] + '-' + components[2] + components[6] + '-' + components[8] + '-' + \
-                                     components[9]
-                        writeFile.write(urlIntelStr)
+                                      + components[1] + '-' + components[2] + components[6] + '-' + components[
+                                          8] + '-' + \
+                                      components[9]
+                        rwWriteFile.write(urlIntelStr)
                     lineCount += 1
-        writeFile.close()
+        rwWriteFile.close()
 
+def main():
+    # Convert the ssl blocklist feeds
+    sslBlockList()
+    # Convert the ja3 blocklist feeds
+    ja3Fingerprint()
+    # Convert the ransomeware blocklist feeds
+    ransomewareBlocklist()
 
 if __name__ == '__main__':
     main()
